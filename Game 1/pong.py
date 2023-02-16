df21037f0c0c5174d1913208253eec74d7400a49
David Bucio <david.bucio.gutierrez@gmail.com>
2023-02-15 23:02:49 -0600

    First Edit

---
+++ Game 1/pong.py
@@ -0,0 +1,32 @@
+# Game 1: Pong
+import turtle
+
+win = turtle.Screen()
+win.title("Pong by @lumberfoot")
+win.bgcolor("black")
+win.setup(width=800, height=600)
+win.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+# Ball
+
+# Main Game Loop
+while True:
+    win.update()
