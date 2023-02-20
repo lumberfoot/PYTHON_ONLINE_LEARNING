2c59882e34739801043d1673d788a37156004141
David Bucio <david.bucio.gutierrez@gmail.com>
2023-02-20 01:26:22 -0600

    Grid

--- Game 2/snake.py
+++ Game 2/snake.py
@@ -27,23 +27,48 @@ class snake(object):
     def reset(self, pos):
         pass
 
-    def addCube(self):
+    def add_cube(self):
         pass
 
     def draw(self, surface):
         pass
 
-def drawGrid(w, rows, surface):
+def draw_grid(w, rows, surface):
+    size_between = w // rows
+    x = 0
+    y = 0
+    
+    for l in range(rows):
+        x = x + size_between
+        y = y + size_between
+        pygame.draw.line(surface, (255, 255, 255), (x, 0), (x, w))
+        pygame.draw.line(surface, (255, 255, 255), (0, y), (w, y))
     pass
 
-def redrawWindow(surface):
+def redraw_window(surface):
+    global width, rows
+    surface.fill((0, 0, 0))
+    draw_grid(width, rows, surface)
+    pygame.display.update()
     pass
 
-def randomSnack(rows, items):
+def random_snack(rows, items):
     pass
 
 def message_box(subject, content):
     pass
 
 def main():
+    global width, rows
+    width = 500
+    rows = 20
+    window = pygame.display.set_mode((width, width))
+    s = snake((255, 0, 0), (10, 10))
+    flag = True
+    clock = pygame.time.Clock()
+
+    while flag:
+        pygame.time.delay(50)
+        clock.tick(10)
+        redraw_window(window)
     pass
